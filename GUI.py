19ca72110596e0c278c498e175ef51cac744653d
kaydeeargh <kdr220721@gmail.com>
2017-09-16 06:08:46 -0500

    added feature to dynamically add text entry

--- GUI.py
+++ GUI.py
@@ -4,72 +4,110 @@ import tkinter.ttk as ttk
 class Main_Window(tk.Frame):
 
     def __init__(self):
-        super().__init__(self)
+        super().__init__()
         self.w_widgets()
         self.widgets()
         self.t_widgets()
         self.au_widgets()
 
     def widgets(self):
-        self.mb = tk.Menubutton(text='file')
-        self.m = tk.Menu(self.mb)
-        self.m.grid(columnspan=1)
+
+        def doNothing():
+            pass
+
+        subMenu = tk.Menu(menu, tearoff=0)
+        menu.add_cascade(label="Add", menu=subMenu)
+        subMenu.add_command(label="W Tapes", command=self.add_wtape)
+        subMenu.add_command(label="T Tapes", command=doNothing)
+        subMenu.add_command(label="AU Tapes", command=doNothing)
+
+    def add_wtape(self):
+
+        def getx():
+            with open(Gui_var.dat) as f:
+                pass
 
 
-    def w_widgets(self):
-
-        # frame for windows tape scans. It's own frame is required for organization and multiples.
-        self.wframe = tk.LabelFrame(labelanchor='n', text="Windows Tapes", height=600, width=150)
-        self.wframe.columnconfigure(0, weight=1)
-        self.wframe.grid(row=10, column=0, padx=5, pady=2)
 
         # entry box for W tape case ID
-        self.wcase = tk.Entry(self.wframe)
+        self.wcase = ttk.Entry(self.wframe)
         self.wcase.insert(0, "Scan Case")
-        self.wcase.grid(row=0, column=0)
+        self.wcase.grid(row=0, column=1, padx=10)
         self.wcase.bind('<Button-1>', self.clear_entry)
 
         # text boxes for windows tape scans
         self.wtext = tk.Text(self.wframe, width=10)
-        self.wtext.grid(row=1, column=0)
+        self.wtext.grid(row=1, column=1, sticky="wens", padx=10, pady=10)
 
-    # method to delete default text when mouse button is pressed
-    def clear_entry(self, event=None):
-        self.wcase.delete(0, 20)
+        x += 1
+
+    def w_widgets(self):
+
+        # frame for windows tape scans. It's own frame is required for organization and multiples.
+        self.wframe = ttk.LabelFrame(labelanchor='n', text="Windows Tapes", height=600, width=150)
+        self.wframe.columnconfigure(0, weight=1)
+        self.wframe.pack(side="left", fill="y")
+
+        # entry box for W tape case ID
+        self.wcase = ttk.Entry(self.wframe)
+        self.wcase.insert(0, "Scan Case")
+        self.wcase.grid(row=0, column=0, padx=10)
+        self.wcase.bind('<Button-1>', self.clear_entry)
+
+        # text boxes for windows tape scans
+        self.wtext = tk.Text(self.wframe, width=10)
+        self.wtext.grid(row=1, column=0, sticky="wens", padx=10, pady=10)
 
     def t_widgets(self):
 
         # frame for T tapes. It's own frame is required for organization and multiples.
-        self.tframe = tk.LabelFrame(labelanchor='n', text="T Tapes", height=600, width=200)
+        self.tframe = ttk.LabelFrame(labelanchor='n', text="T Tapes", height=600, width=200)
         self.tframe.columnconfigure(0, weight=1)
-        self.tframe.grid(row=10, column=1, padx=5, pady=2)
+        self.tframe.pack(side="left", fill="y")
 
         # entry box for T tape case ID
-        self.tcase = tk.Entry(self.tframe)
+        self.tcase = ttk.Entry(self.tframe)
         self.tcase.insert(0, "Scan Case")
-        self.tcase.grid(row=0, column=0)
+        self.tcase.grid(row=0, column=0, padx=10)
+        self.tcase.bind('<Button-1>', self.clear_entry)
 
         # text box for t tape scans
         self.ttext = tk.Text(self.tframe, width=10)
-        self.ttext.grid(row=1, column=0)
+        self.ttext.grid(row=1, column=0, sticky="wens", padx=10, pady=10)
 
     def au_widgets(self):
         # frame for T tapes. It's own frame is required for organization and multiples.
-        self.auframe = tk.LabelFrame(labelanchor='n', text="AU Tapes", height=600, width=200)
+        self.auframe = ttk.LabelFrame(labelanchor='n', text="AU Tapes", height=600, width=200)
         self.auframe.columnconfigure(0, weight=1)
-        self.auframe.grid(row=10, column=3, padx=5, pady=2)
+        self.auframe.pack(side="left", fill="y")
 
         # entry box for T tape case ID
-        self.aucase = tk.Entry(self.auframe)
+        self.aucase = ttk.Entry(self.auframe)
         self.aucase.insert(0, "Scan Case")
-        self.aucase.grid(row=0, column=0)
+        self.aucase.grid(row=0, column=0, padx=10)
+        self.aucase.bind('<Button-1>', self.clear_entry)
 
         # text box for t tape scans
         self.autext = tk.Text(self.auframe, width=10)
-        self.autext.grid(row=1, column=0)
+        self.autext.grid(row=1, column=0, sticky="wens", padx=10, pady=10)
+
+    # method to delete default text when mouse button is pressed
+    def clear_entry(self, event=None):
+        event.widget.delete(0, 'end')
+
+    def add_case(self):
+        pass
 
 
 root = tk.Tk()
 root.geometry('600x600+200+200')
+
+root.style = ttk.Style()
+root.style.theme_use("default")
+
+
+menu = tk.Menu(root)
+root.config(menu=menu)
+
 app = Main_Window()
 app.mainloop()
